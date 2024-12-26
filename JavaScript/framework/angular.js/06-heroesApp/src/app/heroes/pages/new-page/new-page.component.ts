7c01f80534e0184bb512fd21d673be8102312bf4
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-12-26 01:39:34 -0500

    sesion 9: Rutas hija y lazy loading

---
+++ JavaScript/framework/angular.js/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-page',
+  standalone: false,
+
+  templateUrl: './new-page.component.html',
+})
+export class NewPageComponent {
+
+}

9a226edce84056f79e4e69840653742511068003
Rignu1503 <rigoberto.miranda1503@gmail.com>
2026-04-14 15:12:53 -0500

    delete codesignal workspace

--- java/ejercicios/src/main/java/org/rigo/mockitoapp/example/Main.java
+++ java/ejercicios/src/main/java/org/rigo/mockitoapp/example/Main.java
@@ -1,47 +1,37 @@
 package org.rigo.mockitoapp.example;
 
-import java.util.Scanner;
-import java.util.Vector;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        /*
+         * Crea un método en Java que:
+         * 
+         * Reciba un String
+         * Retorne el mismo String pero invertido
+         * 
+         */
 
-        Vector<Integer> numeros = new Vector<>();
-
-        int i = 0;
-
-        System.out.print("Cuantos numero quiere ingresar? ");
-        int ingresados = scanner.nextInt();
-        scanner.nextLine();
-        while (ingresados > i) {
-
-            System.out.print("ingrese un numero ");
-            int numeroIngresado = scanner.nextInt();
-            numeros.add(numeroIngresado);
-            i++;
-        }
-
-        int mayor = numeros.get(0);
-        int menor = numeros.get(0);
-
-
-        scanner.close();
-        for (int j = 0; j < numeros.size(); j++) {
-
-            if (numeros.get(j) > mayor) {
-                mayor = numeros.get(j);
-            }
-            if (numeros.get(j) < menor) {
-                menor = numeros.get(j);
-            }
-        }
-
-        System.out.println("Numero mayor: " + mayor);
-        System.out.println("Numero menor: " + menor);
+        System.out.println(invertedMethod("hola"));
+        System.out.println(invertedMethod("Como estas"));
+        System.out.println(invertedMethod("batman"));
 
     }
+
+    public static String invertedMethod(String string) {
+
+        StringBuilder textInverted = new StringBuilder();
+        if (string == null) {
+            return null;
+        } else {
+
+            for (int i = string.length() - 1; i >= 0; i--) {
+
+                textInverted.append(string.charAt(i));
+
+            }
+        }
+
+        return textInverted.toString();
+    }
+
 }
